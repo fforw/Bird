62c1c8943aa7830bd21fabcc78386198ce916d33
Sven Helmberger <fforw@gmx.de>
2014-02-16 19:25:12 +0100

    Only define browserify config once

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,23 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var watch = require('gulp-watch');
 
+var browserifyConfig = {
+    transform: ["reactify"],
+    debug : !gulp.env.production
+};
+
 // Basic usage
 gulp.task('default', function() {
     // Single entry point to browserify
     gulp.src('src/bird.js')
-        .pipe(browserify({
-            transform: ["reactify"],
-            debug : !gulp.env.production
-        }))
+        .pipe(browserify(browserifyConfig))
         .pipe(gulp.dest("web"));
 });
 
 gulp.task('watch', function () {
     gulp.src('src/bird.js')
         .pipe(watch(function(files) {
-            return files.pipe(browserify({
-                    transform: ["reactify"],
-                    debug : !gulp.env.production
-                }))
+            return files.pipe(browserify(browserifyConfig))
                 .pipe(gulp.dest("web"));
         }));
 });
